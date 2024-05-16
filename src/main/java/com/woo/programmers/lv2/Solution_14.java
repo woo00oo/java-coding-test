2de5d3aed63765602d065c8c5d5764c948ac89e3
woo00oo <96woo94@naver.com>
2024-05-16 20:50:36 +0900

    멀리뛰기

---
+++ src/main/java/com/woo/programmers/lv2/Solution_14.java
@@ -0,0 +1,21 @@
+package com.woo.programmers.lv2;
+
+/**
+ * 멀리뛰기
+ */
+public class Solution_14 {
+    public long solution(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
+
+        return dp[n];
+    }
+}
